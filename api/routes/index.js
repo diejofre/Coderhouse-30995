1475b65d14d105433b61e4537a43ab51a7d70875
diejofre <diego-jofre@live.com.ar>
2022-06-10 10:56:55 -0300

    push api ejemplo after class

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const tweetsRouter = require("./tweets");
-const usersRouter = require("./users");
-
-router.use("/tweets", tweetsRouter)
-router.use("/users", usersRouter)
-
-module.exports = router;
